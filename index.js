9fb40677dcc1f9bd9e012fd5ce1ab7b65586421d
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-21 13:05:22 +0300

    feat: define functions, passing tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+ return drivers.slice(0, 2); 
+}
+
+function returnLastTwoDrivers(drivers){
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => fare => fare * multiplier;
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, selector) => {
+  return selector(drivers)
+}
